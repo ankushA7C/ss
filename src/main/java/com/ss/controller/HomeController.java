b1583ca5d25a660592e80592af238ae2983bf4ff
User <User@DESKTOP-1LFJMF2>
2019-03-04 20:06:02 +0530

    Dynamic users from database

--- src/main/java/com/ss/controller/HomeController.java
+++ src/main/java/com/ss/controller/HomeController.java
@@ -2,8 +2,13 @@ package com.ss.controller;
 
 import java.security.Principal;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,9 +29,17 @@ public class HomeController {
 		return new ModelAndView("home", "userName", loggedInUserName);
 	}
 	
-	@RequestMapping(value = "/logout")
-	public String logoutView() {
-		return "redirect:login";
+	/*
+	 * @RequestMapping(value = "/logout") public String logoutView() { return
+	 * "redirect:login"; }
+	 */
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
 	}
 
 }
--- src/main/java/com/ss/java/WebSecurityConfig.java
+++ src/main/java/com/ss/java/WebSecurityConfig.java
@@ -3,23 +3,25 @@ package com.ss.java;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-		 @Override
+
+	@Override
 	    protected void configure(HttpSecurity http) throws Exception {
 	        http
 	            .authorizeRequests()
-	            .antMatchers("/resources/**","/resources/static/assets/*").permitAll().anyRequest().permitAll()
-	                .antMatchers("/", "/home").permitAll()
-	                //.anyRequest().authenticated()
+	               .antMatchers("/home").authenticated()
+	               //.antMatchers("/").permitAll()
+	                .anyRequest().authenticated()
+	                .antMatchers("/resources/**","/resources/static/assets/*").permitAll().anyRequest().permitAll()
 		            .and()
 	            .formLogin()
 	                .loginPage("/login")
@@ -27,21 +29,53 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	                .permitAll()
 	                .and()
 	            .logout()
-	                .permitAll();
+	            .logoutUrl("/j_spring_security_logout")
+	            .logoutSuccessUrl("/login")
+	             .permitAll();
 	    }
 
-	    @Bean
-	    @Override
-	    public UserDetailsService userDetailsService() {
-	        UserDetails user =
-	             User.withDefaultPasswordEncoder()
-	                .username("user")
-	                .password("12345")
-	                .roles("USER")
-	                .build();
-
-	        return new InMemoryUserDetailsManager(user);
-	    }
+	/* Hard coded user credentials */
+	  
+	  
+	  @Bean
+	  
+	  @Override public UserDetailsService userDetailsService() { UserDetails user =
+	  User.withDefaultPasswordEncoder() .username("user") .password("12345")
+	  .roles("USER") .build();
+	  
+	  return new InMemoryUserDetailsManager(user); }
+	 
+		 
+	/*Dynamic users from database
+	 * 
+	 * 
+	 * @Autowired private DataSource dataSource;
+	 * 
+	 * 
+	 * @Bean
+	 * 
+	 * @Primary
+	 * 
+	 * @ConfigurationProperties("spring.datasource") public DataSourceProperties
+	 * dataSourceProperties() { return new DataSourceProperties(); }
+	 * 
+	 * @Bean
+	 * 
+	 * @ConfigurationProperties("spring.datasource") public HikariDataSource
+	 * dataSource(DataSourceProperties properties) { return
+	 * properties.initializeDataSourceBuilder().type(HikariDataSource.class)
+	 * .build(); }
+	 * 
+	 * @Autowired public void configAuthentication(AuthenticationManagerBuilder
+	 * auth) throws Exception {
+	 * 
+	 * auth.jdbcAuthentication().dataSource(dataSource)
+	 * .authoritiesByUsernameQuery("select username, role from user_roles where username=?"
+	 * )
+	 * .usersByUsernameQuery("select username, password, active from users where username=?"
+	 * ) .passwordEncoder(new Md4PasswordEncoder()); }
+	 */
+	    
 	
 }
 
